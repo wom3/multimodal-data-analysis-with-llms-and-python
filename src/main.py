533030525a845b1e4bfff75f3ca69fa2a8dad793
wom3 <wom2@student.london.ac.uk>
2026-01-19 23:35:12 +0500

    feat(classification): classify texts from CSV and write results to `classified_output.csv`

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ Entry script for your ML project.
 """
 
 import openai
+import pandas as pd
 import argparse
 
 client = openai.OpenAI()
@@ -41,24 +42,36 @@ def call_llm(prompt: str) -> str:
     print(response)
     return response.choices[0].message.content
 
+def classify_text(text: str) -> str:
+    '''
+    Docstring for classify_text
+    
+    :param text: Description
+    :type text: str
+    :return: Classification result
+    :rtype: str
+    '''
+    
+    prompt = create_prompt(text)
+    answer = call_llm(prompt)
+    return answer
+
+
 
 def __main__():
     print("🚀 Project is ready. Start building in src/ !")
     print("Tip: open ML_Workflow.txt for a step-by-step checklist.")
-    parser = argparse.ArgumentParser(description="A text to classify.")
-    parser.add_argument("text", nargs="?", default=None, help="Text to classify")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str, help='Path to the input text file')
     args = parser.parse_args()
 
-    text = args.text
-    if text is None:
-        text = input("Enter text to classify: ").strip()
-        if not text:
-            print("No text provided. Exiting.")
-
-    prompt = create_prompt(text)
-    answer = call_llm(prompt)
-    print(f"Input Text: {text}")
-    print(f"LLM Response: {answer}")
+    df = pd.read_csv(args.path)   
+    df['Sentiment'] = df['Text'].apply(classify_text)
+    df.to_csv('classified_output.csv', index=False)
+    # prompt = create_prompt(text)
+    # answer = call_llm(prompt)
+    # print(f"Input Text: {text}")
+    # print(f"LLM Response: {answer}")
 
 if __name__ == "__main__":
     __main__()
